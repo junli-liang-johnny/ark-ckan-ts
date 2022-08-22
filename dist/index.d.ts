156079204b29f33c552f400c6414e694546e23f7
junli <junli.liang@adaptcentre.ie>
2022-08-22 18:24:40 +0800

    change ts compile options

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './package';
-export * from './resource';
-export * from './user';
-export * from './base';
-export * from './organisation';
--- dist/user.d.ts
+++
@@ -1,7 +0,0 @@
-import { CKANAPIResponse, CKANAPITokenRevokeProps, CKANAPIUserCreateProps, CKANAPIUserGetResponse, CKANAPIUserhowProps, CKANAPIUserListProps, CKANAPIUserRemoveProps, CKANAPIUserShowResponse, CKANAPIUserTokenCreateProps, CKANAPIUserTokenCreateResponse, CKANAPIUserTokenProps, CKANAPIUserTokenResponse, CKANAPIUserUpdateProps } from './types';
-export declare class CKANUserService {
-    static get(type: 'list' | 'show' | 'api_token_list', data: Partial<CKANAPIUserhowProps | CKANAPIUserListProps | CKANAPIUserTokenProps>): Promise<CKANAPIUserGetResponse | CKANAPIUserShowResponse | CKANAPIUserTokenResponse>;
-    static create(type: 'user_create' | 'api_token_create', data: CKANAPIUserCreateProps | CKANAPIUserTokenCreateProps): Promise<CKANAPIUserShowResponse | CKANAPIUserTokenCreateResponse>;
-    static remove(type: 'user_remove' | 'api_token_revoke', data: CKANAPIUserRemoveProps | CKANAPITokenRevokeProps): Promise<CKANAPIResponse>;
-    static update(data: CKANAPIUserUpdateProps): Promise<CKANAPIUserShowResponse>;
-}
