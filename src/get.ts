86810c3d4b39020af9521e8941204b4cc94b1932
Junli Liang <junli.liang@adaptcentre.ie>
2025-02-24 18:13:51 +0000

    updated test

--- src/get.ts
+++ src/get.ts
@@ -47,10 +47,7 @@ const url = "https://ark-evidence.adaptcentre.ie/api/action";
 // formData.append('user', 'testuser');
 // formData.append('name', 'ark-evidence');
 
-// revoke api token
-// formData.append('jti', 'ik3xCN0fdOb7Ir-MI6tGyvgLrpKnc2WfPdYGvS8rqPSu_-mwaSQdON6PNSdruPTapOJrc5wVQ6xzcqWX');
-const apiToken =
-	"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJtOWdESHh0SlRKb3JMWl9sZVdsSmNOczVzRUhFeVRHVm1UVTRJUi04S21nRXBCOHBGVkJHWm1tcFVBbGVNNlZ0aEFKR2Y4c1BMNmdFRDcxRCIsImlhdCI6MTcxNTA5NTU1OX0.SO8BggJzk0sWO7Mu-qPO73jPPFtlLMVEjlq-VC1znxQ";
+const apiToken = "";
 
 // fetch(url + '/group_show?id=se-opengov')
 // fetch('https://demo.ckan.org/api/3/action/group_show?id=test-group')
@@ -60,11 +57,11 @@ const apiToken =
 // fetch(url + '/package_show?id=e89d59e8-6665-49b9-b431-d2adcea2dba3', { headers: { 'Authorization': CKANAPIBase.API_KEY } })
 // fetch(url + '/package_show?id=9a42f4e8-0bb3-42db-bf99-2a2a3f33ff0b', { headers: { 'Authorization': CKANAPIBase.API_KEY } })
 // fetch(url + "/package_list", { headers: { Authorization: apiToken } })
-fetch(url + "/package_list", {
-	headers: { Authorization: CKANAPIBase.API_KEY },
-})
-	.then((res) => res.json())
-	.then((res) => console.log("res: ", res));
+// fetch(url + "/package_list", {
+// 	headers: { Authorization: CKANAPIBase.API_KEY },
+// })
+// 	.then((res) => res.json())
+// 	.then((res) => console.log("res: ", res));
 // fetch(url + '/member_list?id=ukgov')
 // fetch(url + '/organization_show?id=trinity-college-dublin&include_users=true')
 // fetch(url + '/organization_show?id=st-james-hospital&include_users=true')
@@ -81,7 +78,17 @@ fetch(url + "/package_list", {
 // fetch(url + '/package_search?include_private=true') // search package with organisation
 // fetch(url + '/package_search?include_private=true&q=ark&fq=organization:dublin-city-university', { 'headers': { 'Authorization': CKANAPIBase.API_KEY } }) // search package with organisation
 // fetch(url + '/package_collaborator_list?id=ark-ui-design', { 'headers': { 'Authorization': CKANAPIBase.API_KEY } })
-// fetch(url + '/user_list') // list all users
+const formData = new FormData();
+formData.append("order_by", "sysadmin");
+fetch(url + "/user_list", {
+	method: "POST",
+	headers: {
+		Authorization: CKANAPIBase.API_KEY,
+	},
+	body: formData,
+})
+	.then((res) => res.json())
+	.then((res) => console.log(res)); // list all users
 // fetch(url + '/user_create', { 'method': 'POST', 'headers': { 'Authorization': CKANAPIBase.API_KEY }, 'body': formData })
 // fetch(url + '/user_show?id=testuser&include_datasets=true&include_num_followers=true&include_password_hash=true&include_plugin_extras=true', { headers: { 'Authorization': CKANAPIBase.API_KEY } }) // list all users
 // fetch(url + '/resource_show?id=53c18e09-909e-456a-9b46-546e791cd943')
@@ -90,8 +97,36 @@ fetch(url + "/package_list", {
 // fetch(url + '/resource_search?query=package_id:7581a1b4-10b1-43bb-b447-0c35215f4e14')
 // fetch(url + '/organization_member_create', { 'method': 'POST', 'headers': { 'Authorization': CKANAPIBase.API_KEY }, 'body': formData })
 // fetch(url + '/api_token_list?user=testuser', { 'headers': { 'Authorization': CKANAPIBase.API_KEY } })
-// fetch(url + '/api_token_list?user=junli', { 'headers': { 'Authorization': CKANAPIBase.API_KEY } })
-// fetch(url + '/api_token_revoke', { 'method': 'POST', 'headers': { 'Authorization': CKANAPIBase.API_KEY }, 'body': formData })
+// fetch(url + "/api_token_list?user=junli", {
+// 	headers: { Authorization: apiToken },
+// })
+// 	.then((res) => res.json())
+// 	.then((res) => {
+// 		const list = res.result as Array<any>;
+// 		console.log("Total tokens: ", list.length);
+// 		const filtered = list.filter((item) => item.name !== "token-remove");
+// 		const first5 = filtered.slice(0, 50);
+
+// 		for (const item of first5) {
+// 			// console.log("item: ", item);
+// 			const formData = new FormData();
+// 			formData.append("jti", item.id);
+// 			formData.append("token", item.name);
+// 			fetch(url + "/api_token_revoke", {
+// 				method: "POST",
+// 				headers: { Authorization: apiToken },
+// 				body: formData,
+// 			})
+// 				.then((res) => res.json())
+// 				.then((res) => console.log("res: ", res.success));
+// 		}
+// 		console.log("Token list to remove: ", first5.length);
+// 	});
+// fetch(url + "/api_token_revoke", {
+// 	method: "POST",
+// 	headers: { Authorization: apiToken },
+// 	body: JSON.stringify({ token: "4989c48b-1f37-47c0-85ff-66096e1afa04" }),
+// });
 // fetch(url + '/api_token_create', { 'method': 'POST', 'headers': { 'Authorization': CKANAPIBase.API_KEY }, 'body': formData })
 // fetch(url + '/resource_create', {
 //   'method': 'POST',
